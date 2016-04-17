183cd8631a267d23b38f4653a3562efbe3e08bfb
jjlippman <jjlippman@earthlink.net>
2016-04-17 12:09:09 -0600

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,29 @@
 'use strict';
+
+function addIngredient(recipe, ingredient, amounts) {
+  recipe[ingredient] = amounts;
+  return recipe;
+}
+
+function removeIngredient (recipe, ingredient) {
+  delete recipe[ingredient];
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, amounts) {
+  recipe[ingredient] = amounts;
+  return recipe;
+}
+
+
+function readRecipe (recipe) {
+ 
+   console.log("this recipe calls for " + recipe[amounts] + " of " + recipe[ingredient]);
+ 
+
+
+}
+
+
+
+
